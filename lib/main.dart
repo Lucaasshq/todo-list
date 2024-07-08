e72b599459ad51dc75ee31acd296d4ed2ddbacbb
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-07-08 16:18:18 -0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todolist/colors/colors.dart';
 import 'package:todolist/tarefas_page.dart';
 
 void main() {
@@ -12,10 +13,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(
-            seedColor: Colors.white, primary: Colors.yellow),
+            seedColor: Colors.white, primary: AppColors.pretop2),
         useMaterial3: true,
       ),
       home: TarefaPage(title: 'Todo List'),
--- lib/tarefas_page.dart
+++ lib/tarefas_page.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:todolist/colors/colors.dart';
 import 'package:todolist/repository/tarefa_repository.dart';
 import './model/tarefa.dart';
